4ae96b33ab0499d4de1a75b9052beb81a6c45960
johnvenkiah <contact@johnvenkiah.com>
2021-07-12 21:45:34 +0000

    py: add columns functionality to get data from last 5 rows

--- run.py
+++ run.py
@@ -84,6 +84,21 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data as
+    a list of lists
+    """
+    sales = SHEET.worksheet('sales')
+    columns = []
+    for i in range(1, 7):
+        column = sales.col_values(i)
+        columns.append(column[-5:])
+
+    return columns
+
+
 def main():
     """
     Run all program functions
@@ -96,4 +111,6 @@ def main():
 
 
 print('welcome to Love Sandwiches Data Automation\n')
-main()
+# main()
+
+get_last_5_entries_sales()
